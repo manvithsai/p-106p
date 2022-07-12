6e9bc8206d81fb92c3cc356d90b78e313ffddff5
manvithsai <90837315+manvithsai@users.noreply.github.com>
2022-07-12 11:53:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function startclassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassify('https://storage.googleapis.com/tm-model/u02fOYRBx/model.json',modelReady)
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    if(error){
+    console.log(error);
+    }
+    else{
+    console.log(results);
+    random_number_r=Math.floor(Math.random()*255)+1;
+    random_number_g=Math.floor(Math.random()*255)+1;
+    random_number_b=Math.floor(Math.random()*255)+1;
+    document.getElementById("sound").innerHTML="I can hear- "+results[0].label;
+    document.getElementById("accurecy-of-sound").innerHTML="accurecy- "+(results[0].confidence*100).toFixed(2)+"%";
+    document.getElementById("sound").style.color="rgb("+random_number_r+","+ random_number_g+","+random_number_b+")";
+    document.getElementById("accurecy-of-sound").style.color="rgb("+random_number_r+","+ random_number_g+","+random_number_b+")";
+    img=document.getElementById("animal_image");
+    if(results[0].label=="Barking"){
+    img.src='dog.gif';dog=dog+1
+    }
+    else if(results[0].label=="Roaring"){
+        img.src='lion.gif';lion=lion+1
+    }
+    else if(results[0].label=="Meowing"){
+        img.src='cat.jpg';cat=cat+1
+    }
+    else{
+        img.src='bird.gif';bird=bird+1  
+    }
+    }
+    }
